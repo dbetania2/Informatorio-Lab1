44e5218e3e481b5ee8700f19837945ab7a0bc335
dbetania2 <109638715+dbetania2@users.noreply.github.com>
2025-06-06 02:00:50 -0300

    feat: Crea el motor de SQLAlchemy () para la conexion a la base de datos SQLite
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    .git/COMMIT_EDITMSG [unix] (02:11 06/06/2025)      enter the commit message for your changes. Lines starting
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    .git/COMMIT_EDITMSG [unix] (02:11 06/06/2025)      	database/

--- persistencia/engine.py
+++ persistencia/engine.py
@@ -1,1 +1,10 @@
-# Configuración del motor de SQLAlchemy
+from sqlalchemy import create_engine
+
+# ruta a base de datos sqlite.
+database_url = "sqlite:///database/recolector.db"
+
+
+#  motor de sqlalchemy..
+# 'echo=false' para no imprirmi cada consulta sql en la consola .
+#  'echo=true' para ver todas las consultas sql en la consola.
+engine = create_engine(database_url, echo=False)
